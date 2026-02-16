522158c93d60f08818b63651a51f6e65bcb90a4e
Z-Denis <zakari.denis@gmail.com>
2026-02-16 15:21:52 +0100

    update exports

--- src/mgd/dataset/__init__.py
+++ src/mgd/dataset/__init__.py
@@ -3,9 +3,17 @@
 from .dataloader import GraphBatchLoader
 from .encoding import encode_molecule
 from .utils import GraphBatch
+from .chemistry import ChemistrySpec, CHEMISTRIES, DEFAULT_CHEMISTRY, get_chemistry
+from .qm9 import QM9_IMPLICIT_H, QM9_EXPLICIT_H
 
 __all__ = [
     "GraphBatchLoader",
     "GraphBatch",
     "encode_molecule",
+    "ChemistrySpec",
+    "CHEMISTRIES",
+    "QM9_IMPLICIT_H",
+    "QM9_EXPLICIT_H",
+    "DEFAULT_CHEMISTRY",
+    "get_chemistry",
 ]
